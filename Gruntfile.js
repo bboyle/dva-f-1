3951b0ff8a09c9acda900116aac2776a9908eed9
Ben Boyle <benjamins.boyle@gmail.com>
2016-04-02 16:11:34 +1000

    style: single quotes preferred

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,7 +34,7 @@ module.exports = function( grunt ) {
 		protractor: {
 			options: {
 				keepAlive: true,
-				configFile: "protractor.conf.js"
+				configFile: 'protractor.conf.js'
 			},
 			auto: {
 				keepAlive: true,
