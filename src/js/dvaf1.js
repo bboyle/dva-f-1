0170ad442ef4e85d52157605fccaf0a4988a0115
Ben Boyle <benjamins.boyle@gmail.com>
2016-04-13 11:24:49 +1000

    basic flow from begin to story page
    - add continue button (form submit) handler
    - add template for begin page
    - update sequence number for story template

--- src/js/dvaf1.js
+++ src/js/dvaf1.js
@@ -5,8 +5,17 @@ $(function( $ ) {
 	'use strict';
 
 	var formView = $( '#dvaf1-form-view' );
-	var template = Handlebars.compile( $( '#dvaf1-story-template' ).html() );
+	var template = Handlebars.compile( $( '#dvaf1-begin-template' ).html() );
 
 	formView.html( template );
+
+
+	// handle form view navigation
+	formView.on( 'submit', function(event) {
+		event.preventDefault();
+
+		template = Handlebars.compile( $( '#dvaf1-story-template' ).html() );
+		formView.html( template );
+	});
 
 });
--- test/dvaf1.spec.js
+++ test/dvaf1.spec.js
@@ -1,18 +1,38 @@
 'use strict';
 
-describe('story page', function() {
+describe( 'begin page', function() {
 
 	beforeEach(function() {
 		browser.ignoreSynchronization = true;
 		browser.get( 'http://localhost:8000/src/' );
 	});
 
-	it('should have a title', function() {
+	it( 'should have a title', function() {
+		expect( browser.getTitle() ).toEqual( 'Prepare your application for a domestic violence protection order' );
+		expect( element( by.css( 'h2' )).getText() ).toEqual( 'Before you begin' );
+	});
+
+	it( 'should have a continue button', function() {
+		expect( element( by.css( 'button' )).getText() ).toEqual( 'Continue' );
+	});
+});
+
+
+describe( 'story page', function() {
+
+	beforeEach(function() {
+		browser.ignoreSynchronization = true;
+		browser.get( 'http://localhost:8000/src/' );
+		// continue from begin to story page
+		element( by.css( 'button' )).click();
+	});
+
+	it( 'should have a title', function() {
 		expect( browser.getTitle() ).toEqual( 'Prepare your application for a domestic violence protection order' );
 		expect( element( by.css( 'h1' )).getText() ).toEqual( 'Prepare your application for a domestic violence protection order' );
 	});
 
-	it('should ask who needs protection', function() {
+	it( 'should ask who needs protection', function() {
 		var label = element( by.css( 'label '));
 		expect( label.getText() ).toEqual( 'Do you need protection?' );
 	});
